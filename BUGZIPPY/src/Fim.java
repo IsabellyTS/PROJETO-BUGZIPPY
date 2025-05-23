128dc911252b855674d3fba8b0356e159531c097
bernardo sinhorelli <182685171+BeSinhorelli@users.noreply.github.com>
2025-05-22 21:25:28 -0300

    Add files via upload

---
+++ BUGZIPPY/src/Fim.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class Fim {
+public Fim(){
+}
+CadastraUser user = new CadastraUser();
+    public void ExibeRanking(String nome, int acertos){
+        JOptionPane.showMessageDialog(null,"PARABENSSSSSS" + "MUITO PARABENSSSS\n UHULLLLLLLLLLLLL🎉🎉🎉🎉🎉🎉🎉🎉\n" + "\nNome: " + user.getNome() +"\nEmail: " + user.getEmail());
+        //Conferir o ranking
+    }
+}
