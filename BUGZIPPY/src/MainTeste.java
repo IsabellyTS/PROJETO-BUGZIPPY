217fcbee9d38471d34a59ab293865898478c586c
bernardo sinhorelli pristupa <bernardosinhorelli348@gmail.com>
2025-05-25 14:38:30 -0300

    testando

--- BUGZIPPY/src/MainTeste.java
+++ BUGZIPPY/src/MainTeste.java
@@ -2,6 +2,8 @@ public class MainTeste {
     public static void main(String[] args) {
         Nivel03 nivel03 = new Nivel03();
         Nivel04 nivel04 = new Nivel04();
-        nivel04.ChamaNivel4();
+        Nivel08 nivel08 = new Nivel08();
+        String nome = "teste";
+        nivel08.ChamaNivel8(nome);
     }
 }
