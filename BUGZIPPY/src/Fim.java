e9f1c8973104cfd4172fed2f22479a099c02372b
bernardo sinhorelli pristupa <bernardosinhorelli348@gmail.com>
2025-05-26 00:06:03 -0300

    Adicionando o spoiler do proximo jogo

--- BUGZIPPY/src/Fim.java
+++ BUGZIPPY/src/Fim.java
@@ -5,5 +5,6 @@ public class Fim {
         frame.setAlwaysOnTop(true);
         String msg = "\uD835\uDE71\uD835\uDE84\uD835\uDE76\uD835\uDE89\uD835\uDE78\uD835\uDE7F\uD835\uDE7F\uD835\uDE88: \uD835\uDE72\uD835\uDE98\uD835\uDE96\uD835\uDE98 \uD835\uDE7F\uD835\uDE9B\uD835\uDE98\uD835\uDE90\uD835\uDE9B\uD835\uDE8A\uD835\uDE96\uD835\uDE8A\uD835\uDE9B" + "🐜";
         JOptionPane.showMessageDialog(frame, "PARABENSSSSSS "+nome+" UHULLLLLLLLLLLL\nVOCE TERMINOU O [" + msg+"]\n🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉" );
+        JOptionPane.showMessageDialog(frame, "Em breve, o próximo jogo será sobre: Orientação a Objetos !!!\nAté a proxima 👋👋👋");
     }
 }
