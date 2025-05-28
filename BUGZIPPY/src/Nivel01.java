8d56deece346b8f0274f287e8e637a79ccedefc6
IsabellyTS <isabellytrindades07@gmail.com>
2025-05-28 17:35:04 -0300

    Update Nivel01.java

--- BUGZIPPY/src/Nivel01.java
+++ BUGZIPPY/src/Nivel01.java
@@ -13,15 +13,17 @@ public class Nivel01 {
 
         ArrayList<String> perguntasN3 = new ArrayList<>();
         String p1 = "O que é uma variável?\n" +
+            "[Capítulo 2 – Página 37]\n\n" +
                 " 1) Um botão secreto no teclado\n" +
-                " 2) Um lugar para guardar valores, como números e textos\n" +
+                " 2) Um lugar para guardar valores, como números e textos \n" +
                 " 3) Um tipo de erro\n" +
                 " 4) Uma função mágica\n" +
                 " 5) Um comando que fecha o programa\n" +
                 " 6) Um vírus de computador\n";
 
         String p2 = "Qual linha cria uma variável inteira com valor 12?\n" +
-                " 1) int idade = 12;\n" +
+            "[Capítulo 2 – Página 37-38]\n\n" +
+                " 1) int idade = 12; \n" +
                 " 2) idade = int 12;\n" +
                 " 3) idade := 12;\n" +
                 " 4) String idade = 12;\n" +
@@ -29,59 +31,67 @@ public class Nivel01 {
                 " 6) idade int = '12';\n";
 
         String p3 =  "Como somar os valores das variáveis a e b e guardar o resultado?\n" +
+                "[Capítulo 2 – Página 41-42]\n\n" +
                 " 1) total = a mais b\n" +
                 " 2) String total = a + b\n" +
                 " 3) int total = a + b\n" +
                 " 4) char total = a + b\n";
 
         String p4 = "O tipo boolean guarda que tipo de informação?\n" +
+            "[Capítulo 2 – Página 37-38]\n\n" +
                 " 1) Números inteiros\n" +
                 " 2) Letras\n" +
                 " 3) Palavras longas\n" +
-                " 4) Verdadeiro ou falso\n" +
+                " 4) Verdadeiro ou falso \n" +
                 " 5) Fotos\n" +
                 " 6) Vídeos\n";
 
         String p5 = "Se eu tenho 10 balas e como 3, qual é a fórmula para saber quantas sobraram?\n" +
+                 "[Capítulo 2 – Página 41-42]\n\n" +
                 " 1) int sobrou = 10 + 3\n" +
                 " 2) int sobrou = 10 * 3\n" +
                 " 3) int sobrou = 10 - 3\n" +
                 " 4) int sobrou = 10 / 3\n";
 
         String p6 = "Qual dessas é uma forma correta de criar uma variável?\n" +
+                   "[Capítulo 2 – Página 37-38]\n\n" +
                 " 1) 12 idade = int;\n" +
                 " 2) idade: int = 12;\n" +
                 " 3) var idade = 12;\n" +
-                " 4) int idade = 12;\n" +
+                " 4) int idade = 12; \n" +
                 " 5) idade := 12;\n" +
                 " 6) idade = '12';\n";
 
         String p7 = "Como juntar os valores 5 e 3 para fazer uma subtração?\n" +
+            "[Capítulo 2 – Página 41-42]\n\n" +
                 " 1) int resultado = 5 - 3\n" +
                 " 2) int resultado = 5 + 3\n" +
                 " 3) int resultado = 5 * 3\n" +
                 " 4) int resultado = 5 / 3\n";
 
         String p8 =  "Para que serve uma variável além de guardar valores?\n" +
+            "[Capítulo 2 – Página 37]\n\n" +
                 " 1) Só para escrever o nome do jogador\n" +
                 " 2) Para decorar o código\n" +
                 " 3) Para guardar imagens\n" +
-                " 4) Para usar em contas e decisões\n" +
+                " 4) Para usar em contas e decisões \n" +
                 " 5) Para imprimir na tela com println\n" +
                 " 6) Para fazer desenhos\n";
 
         String p9 =  "O que significa a palavra int em Java?\n" +
+            "[Capítulo 2 – Página 37-38]\n\n" +
                 " 1) Iniciar um programa\n" +
                 " 2) Mostrar uma mensagem\n" +
-                " 3) Guardar um número inteiro\n" +
+                " 3) Guardar um número inteiro \n" +
                 " 4) Criar uma nova janela\n" +
                 " 5) Ligar a internet\n" +
                 " 6) Apagar um valor\n";
 
         String p10 =  "Qual nome é válido para uma variável?\n" +
+            "[Capítulo 2 – Página 36]\n\n" +
                 " 1) 1nome\n" +
                 " 2) meu nome\n" +
-                " 3) nome_do_usuario\n" +
+                " 3) nome_do_usuario \n" +
                 " 4) nome-do-usuário\n" +
                 " 5) nome@com\n" +
                 " 6) class\n";
@@ -125,7 +135,6 @@ public class Nivel01 {
                         "-------------------------------------------------------------\n\n" +
 
                         ">> O que são Variáveis?\n" +
-                        "[Capítulo 2 – Página 37]\n\n" +
                         "Variáveis são como caixinhas que guardam informações.\n" +
                         "Por exemplo, podemos ter uma variável chamada idade que guarda o número 12,\n" +
                         "ou uma chamada nome que guarda o texto \"Maria\".\n\n" +
@@ -159,7 +168,6 @@ public class Nivel01 {
                         "-------------------------------------------------------------\n\n" +
 
                         ">> O que são Fórmulas?\n" +
-                        "[Capítulo 2 – Página 41-42]\n\n" +
                         "Fórmulas são operações que usamos para calcular algum valor usando variáveis.\n" +
                         "É como fazer contas com números que estão guardados nas caixinhas (variáveis).\n\n" +
                         "\"Operador\\t     O que faz\\t           Exemplo (a = 6, b = 3)\n"+
