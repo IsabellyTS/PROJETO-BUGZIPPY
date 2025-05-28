469e0f7ec41bf5d8a29f2b91c41f4ee543e3a42d
aruwer <amandamaffini@gmail.com>
2025-05-28 17:58:56 -0300

    Atualização nível 3

--- BUGZIPPY/src/Nivel03.java
+++ BUGZIPPY/src/Nivel03.java
@@ -14,69 +14,88 @@ public class Nivel03 {
         String p1 = "\n[Capítulo 5, pág. 172]\n" +
                 "O que o comando switch faz no Java? Escolha a melhor explicação:\n" +
                 "Imagine que você tem várias opções e precisa escolher apenas uma, como em um cardápio. O switch ajuda o computador a escolher o que fazer com base em um valor.\n" +
-                "1) Repete um bloco de código várias vezes\n" +
-                "2) Compara uma variável com vários valores possíveis\n" +
-                "3) Cria um novo método no programa\n" +
-                "4) Serve para fazer contas de matemática\n" +
-                "5) Mostra mensagens na tela\n" +
-                "6) Deixa o programa mais lento\n" +
-                "escreva: ";
+                " 1) Repete um bloco de código várias vezes\n" +
+                " 2) Compara uma variável com vários valores possíveis\n" +
+                " 3) Cria um novo método no programa\n" +
+                " 4) Serve para fazer contas de matemática\n" +
+                " 5) Mostra mensagens na tela\n" +
+                " 6) Deixa o programa mais lento\n";
         String p2 = "\n[Capítulo 5, pág. 173]\n" +
                 "Para que serve o comando break dentro de um switch?\n" +
-                "1) Para fazer uma pausa no teclado\n" +
-                "2) Para encerrar o programa inteiro\n" +
-                "3) Para sair do switch depois que a opção correta foi encontrada\n" +
-                "4) Para repetir o mesmo código várias vezes\n" +
-                "5) Para pular para o próximo case\n" +
-                "6) Para escrever uma nova variável\n" +
-                "escreva: ";
+                " 1) Para fazer uma pausa no teclado\n" +
+                " 2) Para encerrar o programa inteiro\n" +
+                " 3) Para sair do switch depois que a opção correta foi encontrada\n" +
+                " 4) Para repetir o mesmo código várias vezes\n" +
+                " 5) Para pular para o próximo case\n" +
+                " 6) Para escrever uma nova variável\n";
         String p3 = "\n[Capítulo 5, pág. 173]\n" +
                 "O que acontece se esquecermos o break dentro do switch?\n" +
-                "1) Ele para automaticamente\n" +
-                "2) Mostra um erro na tela\n" +
-                "3) Executa os cases abaixo\n" +
-                "4) Ignora o switch completamente\n" +
-                "5) Ele volta para o início do programa\n" +
-                "6) Nada acontece\n" +
-                "escreva: ";
+                " 1) Ele para automaticamente\n" +
+                " 2) Mostra um erro na tela\n" +
+                " 3) Executa os cases abaixo\n" +
+                " 4) Ignora o switch completamente\n" +
+                " 5) Ele volta para o início do programa\n" +
+                " 6) Nada acontece\n";
         String p4 = "\n[Capítulo 5, pág. 172]\n" +
                 "Qual destas opções mostra a forma correta de escrever um switch básico?\n" +
-                "1) switch x { \ncase 1: \nbreak; }\n" +
-                "2) switch(x) { \ncase 1: \nSystem.out.println(\"Um\"); \nbreak; }\n" +
-                "3) switch(x) [ \ncase 1; \nbreak; ]\n" +
-                "4) switch = x { 1 -> System.out.println(\"Um\"); }\n" +
-                "5) switch(x) case 1: System.out.println(\"Um\")\n" +
-                "6) switched(x) { case 1: break; }\n" +
-                "escreva: ";
+                " 1) switch x { \ncase 1: \nbreak; }\n" +
+                " 2) switch(x) { \ncase 1: \nSystem.out.println(\"Um\"); \nbreak; }\n" +
+                " 3) switch(x) [ \ncase 1; \nbreak; ]\n" +
+                " 4) switch = x { 1 -> System.out.println(\"Um\"); }\n" +
+                " 5) switch(x) case 1: System.out.println(\"Um\")\n" +
+                " 6) switched(x) { case 1: break; }\n";
         String p5 = "\n[Capítulo 5, pág. 174]\n" +
-                "O que será impresso na tela?\nint dia = 2;\nswitch (dia) {\n    case 1: System.out.println(\"Domingo\"); break;\n    case 2: System.out.println(\"Segunda\"); break;\n}\n" +
-                "1) Domingo\n2) Segunda\n3) Nada\n4) Erro de compilação\n5) Terça\n6) Segunda e Terça\n" +
-                "escreva: ";
+                "O que será impresso na tela?\n" +
+                "int dia = 2;\n" +
+                "switch (dia) {\n    " +
+                "   case 1: System.out.println(\"Domingo\"); break;\n    " +
+                "   case 2: System.out.println(\"Segunda\"); break;\n}\n" +
+                " 1) Domingo\n" +
+                " 2) Segunda\n" +
+                " 3) Nada\n" +
+                " 4) Erro de compilação\n" +
+                " 5) Terça\n" +
+                " 6) Segunda e Terça\n";
         String p6 = "\n[Capítulo 5, pág. 174]\n" +
                 "O que acontece com este código?\nint x = 3;\nswitch(x) {\n case 1: System.out.println(\"Um\");\n case 2: System.out.println(\"Dois\");\n case 3: System.out.println(\"Três\");\n}\n" +
-                "1) Imprime “Três”\n2) Não imprime nada\n3) Imprime “Três” apenas\n4) Imprime “Três” e dá erro\n5) Imprime “Três”, “Dois” e “Um”\n6) Imprime “Três” e continua com os outros case\n" +
-                "escreva: ";
+                " 1) Imprime “Três”\n" +
+                " 2) Não imprime nada\n" +
+                " 3) Imprime “Três” apenas\n" +
+                " 4) Imprime “Três” e dá erro\n" +
+                " 5) Imprime “Três”, “Dois” e “Um”\n" +
+                " 6) Imprime “Três” e continua com os outros case\n";
         String p7 = "\n[Capítulo 5, pág. 172]\n" +
                 "Qual tipo de variável é mais usada no switch?\n" +
-                "1) boolean\n2) double\n3) float\n4) int\n5) String apenas\n6) long apenas\n" +
-                "escreva: ";
+                " 1) boolean\n" +
+                " 2) double\n" +
+                " 3) float\n" +
+                " 4) int\n" +
+                " 5) String apenas\n" +
+                " 6) long apenas\n";
         String p8 = "\n[Capítulo 5, pág. 174]\n" +
                 "Complete a frase: o switch é melhor usado quando...\n" +
-                "1) Temos poucas opções de escolha\n2) Queremos repetir várias vezes\n3) Precisamos verificar muitos valores de uma variável\n" +
-                "4) Queremos comparar dois valores apenas\n5) Queremos que o código seja mais difícil de ler\n6) Queremos usar apenas if e else\n" +
-                "escreva: ";
+                " 1) Temos poucas opções de escolha\n" +
+                " 2) Queremos repetir várias vezes\n" +
+                " 3) Precisamos verificar muitos valores de uma variável\n" +
+                " 4) Queremos comparar dois valores apenas\n" +
+                " 5) Queremos que o código seja mais difícil de ler\n" +
+                " 6) Queremos usar apenas if e else\n";
         String p9 = "\n[Capítulo 5, pág. 175]\n" +
                 "Qual dessas situações é uma boa ideia para usar switch em vez de if?\n" +
-                "1) Quando queremos somar dois números\n2) Quando temos várias opções fixas, como dias da semana\n" +
-                "3) Quando temos que verificar se um número é par\n4) Quando lemos um número aleatório\n" +
-                "5) Quando lidamos com textos longos\n6) Quando precisamos de loops (for ou while)\n" +
-                "escreva: ";
+                " 1) Quando queremos somar dois números\n" +
+                " 2) Quando temos várias opções fixas, como dias da semana\n" +
+                " 3) Quando temos que verificar se um número é par\n" +
+                " 4) Quando lemos um número aleatório\n" +
+                " 5) Quando lidamos com textos longos\n" +
+                " 6) Quando precisamos de loops (for ou while)\n";
         String p10 = "\n[Capítulo 5, pág. 176]\n" +
                 "Em qual dessas situações o uso do switch é mais indicado?\n" +
-                "1) Para calcular a média de notas de um aluno\n2) Para verificar se um número é maior que 100\n" +
-                "3) Para escolher uma ação com base no botão pressionado (A, B, C...)\n4) Para contar até 10 com um loop\n" +
-                "5) Para comparar nomes completos\n6) Para validar se uma senha tem mais de 8 caracteres\n" +
-                "escreva: ";
+                " 1) Para calcular a média de notas de um aluno\n" +
+                " 2) Para verificar se um número é maior que 100\n" +
+                " 3) Para escolher uma ação com base no botão pressionado (A, B, C...)\n" +
+                " 4) Para contar até 10 com um loop\n" +
+                " 5) Para comparar nomes completos\n" +
+                " 6) Para validar se uma senha tem mais de 8 caracteres\n";
         perguntasN3.add(p1);
         perguntasN3.add(p2);
         perguntasN3.add(p3);
