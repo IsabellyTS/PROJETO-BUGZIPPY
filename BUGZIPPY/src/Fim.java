97a89115a6cbf14cbb6eb7d6cc92ba3305036c8f
IsabellyTS <isabellytrindades07@gmail.com>
2025-05-29 16:14:52 -0300

    Update Fim.java

--- BUGZIPPY/src/Fim.java
+++ BUGZIPPY/src/Fim.java
@@ -3,8 +3,12 @@ public class Fim {
     public void Parabenizar(String nome){
         JFrame frame = new JFrame();
         frame.setAlwaysOnTop(true);
-        String msg = "\uD835\uDE71\uD835\uDE84\uD835\uDE76\uD835\uDE89\uD835\uDE78\uD835\uDE7F\uD835\uDE7F\uD835\uDE88: \uD835\uDE72\uD835\uDE98\uD835\uDE96\uD835\uDE98 \uD835\uDE7F\uD835\uDE9B\uD835\uDE98\uD835\uDE90\uD835\uDE9B\uD835\uDE8A\uD835\uDE96\uD835\uDE8A\uD835\uDE9B" + "🐜";
-        JOptionPane.showMessageDialog(frame, "PARABENSSSSSS "+nome+" UHULLLLLLLLLLLL\nVOCE TERMINOU O [" + msg+"]\n🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉" );
+        System.out.println("\uD835\uDC77\uD835\uDC68\uD835\uDC79\uD835\uDC68\uD835\uDC69\uD835\uDC6C\uD835\uDC75\uD835\uDC7A, \uD835\uDC7D\uD835\uDC76\uD835\uDC6A\uD835\uDC6C \uD835\uDC6A\uD835\uDC76\uD835\uDC75\uD835\uDC7A\uD835\uDC6C\uD835\uDC6E\uD835\uDC7C\uD835\uDC70\uD835\uDC7C \uD835\uDC73\uD835\uDC6C\uD835\uDC7D\uD835\uDC68\uD835\uDC79 \uD835\uDC68 \uD835\uDC69\uD835\uDC7C\uD835\uDC6E\uD835\uDC81\uD835\uDC70\uD835\uDC77\uD835\uDC77\uD835\uDC80 \uD835\uDC77\uD835\uDC79\uD835\uDC76 \uD835\uDC7A\uD835\uDC6C\uD835\uDC7C \uD835\uDC6D\uD835\uDC76\uD835\uDC79\uD835\uDC74\uD835\uDC70\uD835\uDC6E\uD835\uDC7C\uD835\uDC6C\uD835\uDC70\uD835\uDC79\uD835\uDC76!! \\n\uD80C\uDC83 ࣪˖ ִֶָ་༘࿐\uD80C\uDDA4");
+        JOptionPane.showMessageDialog(frame, "ミ★ \uD835\uDE19\uD835\uDE22\uD835\uDE2F\uD835\uDE2C\uD835\uDE2A\uD835\uDE2F\uD835\uDE28 ★彡\n" +
+                "\\nTop 3 Nacional:\n" +
+                "\\n♕. \n" +nome+
+                "\\n2.Josèfino\n" +
+                "\\n3.Virgínia" );
         JOptionPane.showMessageDialog(frame, "Em breve, o próximo jogo será sobre: Orientação a Objetos !!!\nAté a proxima 👋👋👋");
     }
 }
