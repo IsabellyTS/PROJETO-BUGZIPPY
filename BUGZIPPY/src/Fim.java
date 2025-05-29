9f14a8fd749af555f59f706c5e3a6aa78460ab5a
Amanda Ruwer <amandamaffini@gmail.com>
2025-05-29 19:16:18 -0300

    Update Fim.java

--- BUGZIPPY/src/Fim.java
+++ BUGZIPPY/src/Fim.java
@@ -4,7 +4,7 @@ public class Fim {
         JFrame frame = new JFrame();
         frame.setAlwaysOnTop(true);
         String msg = "\uD835\uDE71\uD835\uDE84\uD835\uDE76\uD835\uDE89\uD835\uDE78\uD835\uDE7F\uD835\uDE7F\uD835\uDE88: \uD835\uDE72\uD835\uDE98\uD835\uDE96\uD835\uDE98 \uD835\uDE7F\uD835\uDE9B\uD835\uDE98\uD835\uDE90\uD835\uDE9B\uD835\uDE8A\uD835\uDE96\uD835\uDE8A\uD835\uDE9B" + "🐜";
-        JOptionPane.showMessageDialog(frame, "Ranking:  \n01. " + nome+ "\n2.José \n3.Maria" );
+        JOptionPane.showMessageDialog(frame, msg + "\nRanking:  \n01. " + nome+ "\n2.José \n3.Maria" );
         JOptionPane.showMessageDialog(frame, "Em breve, o próximo jogo será sobre: Orientação a Objetos !!!\nAté a proxima 👋👋👋");
     }
 }
