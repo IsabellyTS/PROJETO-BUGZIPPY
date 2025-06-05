17bcf9a64d3058d4a48388e5be2462a2e53ab454
BeSinhorelli <bernardosinhorelli348@gmail.com>
2025-06-05 19:22:47 -0300

    adicionando perguntasN3/N4/N8

--- BUGZIPPY/src/Perguntas.java
+++ BUGZIPPY/src/Perguntas.java
@@ -76,4 +76,237 @@ public class Perguntas {
                 " 5) nome@com\n" +
                 " 6) class\n";
     }
+    public void perguntasN3(){
+       String p1 = "\n[Capítulo 5, pág. 172]\n" +
+               "O que o comando switch faz no Java? Escolha a melhor explicação:\n" +
+               "Imagine que você tem várias opções e precisa escolher apenas uma, como em um cardápio. O switch ajuda o computador a escolher o que fazer com base em um valor.\n" +
+               "1) Repete um bloco de código várias vezes\n" +
+               "2) Compara uma variável com vários valores possíveis\n" +
+               "3) Cria um novo método no programa\n" +
+               "4) Serve para fazer contas de matemática\n" +
+               "5) Mostra mensagens na tela\n" +
+               "6) Deixa o programa mais lento\n" +
+               "escreva: ";
+       String p2 = "\n[Capítulo 5, pág. 173]\n" +
+               "Para que serve o comando break dentro de um switch?\n" +
+               "1) Para fazer uma pausa no teclado\n" +
+               "2) Para encerrar o programa inteiro\n" +
+               "3) Para sair do switch depois que a opção correta foi encontrada\n" +
+               "4) Para repetir o mesmo código várias vezes\n" +
+               "5) Para pular para o próximo case\n" +
+               "6) Para escrever uma nova variável\n" +
+               "escreva: ";
+       String p3 = "\n[Capítulo 5, pág. 173]\n" +
+               "O que acontece se esquecermos o break dentro do switch?\n" +
+               "1) Ele para automaticamente\n" +
+               "2) Mostra um erro na tela\n" +
+               "3) Executa os cases abaixo\n" +
+               "4) Ignora o switch completamente\n" +
+               "5) Ele volta para o início do programa\n" +
+               "6) Nada acontece\n" +
+               "escreva: ";
+       String p4 = "\n[Capítulo 5, pág. 172]\n" +
+               "Qual destas opções mostra a forma correta de escrever um switch básico?\n" +
+               "1) switch x { \ncase 1: \nbreak; }\n" +
+               "2) switch(x) { \ncase 1: \nSystem.out.println(\"Um\"); \nbreak; }\n" +
+               "3) switch(x) [ \ncase 1; \nbreak; ]\n" +
+               "4) switch = x { 1 -> System.out.println(\"Um\"); }\n" +
+               "5) switch(x) case 1: System.out.println(\"Um\")\n" +
+               "6) switched(x) { case 1: break; }\n" +
+               "escreva: ";
+       String p5 = "\n[Capítulo 5, pág. 174]\n" +
+               "O que será impresso na tela?\nint dia = 2;\nswitch (dia) {\n    case 1: System.out.println(\"Domingo\"); break;\n    case 2: System.out.println(\"Segunda\"); break;\n}\n" +
+               "1) Domingo\n2) Segunda\n3) Nada\n4) Erro de compilação\n5) Terça\n6) Segunda e Terça\n" +
+               "escreva: ";
+       String p6 = "\n[Capítulo 5, pág. 174]\n" +
+               "O que acontece com este código?\nint x = 3;\nswitch(x) {\n case 1: System.out.println(\"Um\");\n case 2: System.out.println(\"Dois\");\n case 3: System.out.println(\"Três\");\n}\n" +
+               "1) Imprime “Três”\n2) Não imprime nada\n3) Imprime “Três” apenas\n4) Imprime “Três” e dá erro\n5) Imprime “Três”, “Dois” e “Um”\n6) Imprime “Três” e continua com os outros case\n" +
+               "escreva: ";
+       String p7 = "\n[Capítulo 5, pág. 172]\n" +
+               "Qual tipo de variável é mais usada no switch?\n" +
+               "1) boolean\n2) double\n3) float\n4) int\n5) String apenas\n6) long apenas\n" +
+               "escreva: ";
+       String p8 = "\n[Capítulo 5, pág. 174]\n" +
+               "Complete a frase: o switch é melhor usado quando...\n" +
+               "1) Temos poucas opções de escolha\n2) Queremos repetir várias vezes\n3) Precisamos verificar muitos valores de uma variável\n" +
+               "4) Queremos comparar dois valores apenas\n5) Queremos que o código seja mais difícil de ler\n6) Queremos usar apenas if e else\n" +
+               "escreva: ";
+       String p9 = "\n[Capítulo 5, pág. 175]\n" +
+               "Qual dessas situações é uma boa ideia para usar switch em vez de if?\n" +
+               "1) Quando queremos somar dois números\n2) Quando temos várias opções fixas, como dias da semana\n" +
+               "3) Quando temos que verificar se um número é par\n4) Quando lemos um número aleatório\n" +
+               "5) Quando lidamos com textos longos\n6) Quando precisamos de loops (for ou while)\n" +
+               "escreva: ";
+       String p10 = "\n[Capítulo 5, pág. 176]\n" +
+               "Em qual dessas situações o uso do switch é mais indicado?\n" +
+               "1) Para calcular a média de notas de um aluno\n2) Para verificar se um número é maior que 100\n" +
+               "3) Para escolher uma ação com base no botão pressionado (A, B, C...)\n4) Para contar até 10 com um loop\n" +
+               "5) Para comparar nomes completos\n6) Para validar se uma senha tem mais de 8 caracteres\n" +
+               "escreva: ";
+    }
+   public void perguntasN4(){
+      String p1 = "\n[Capítulo 5, pág. 172] Para que serve o laço for em Java?\n" +
+              "1) Criar uma nova janela\n" +
+              "2) Repetir um bloco de código um número definido de vezes\n" +
+              "3) Fazer cálculos com variáveis\n" +
+              "4) Ler dados do usuário\n" +
+              "5) Mostrar mensagens de erro\n" +
+              "6) Organizar as pastas do projeto\n";
+
+      String p2 = "\n[Capítulo 5, pág. 173] Qual é a estrutura básica de um laço for em Java?\n" +
+              "1) for (início; condição; atualização)\n" +
+              "2) for {início, condição, atualização}\n" +
+              "3) loop (início:condição:atualização)\n" +
+              "4) for-in (variável)\n" +
+              "5) enquanto (condição)\n" +
+              "6) se (condição) então faça\n";
+
+      String p3 = "\n[Capítulo 5, pág. 175] O que será impresso por: for (int i = 1; i <= 3; i++) { System.out.print(i + \" \"); }\n" +
+              "1) 0 1 2\n" +
+              "2) 1 2 3\n" +
+              "3) 1 2 3 4\n" +
+              "4) 0 1 2 3\n" +
+              "5) 2 3 4\n" +
+              "6) Nada, dá erro\n";
+
+      String p4 = "\n[Capítulo 5, pág. 174] Em um for, qual parte verifica se o laço continua?\n" +
+              "1) A parte do início\n" +
+              "2) A parte da atualização\n" +
+              "3) A parte da condição\n" +
+              "4) O nome do laço\n" +
+              "5) A variável i\n" +
+              "6) A palavra for\n";
+
+      String p5 = "\n[Capítulo 5, pág. 176] Qual alternativa cria um laço que conta de 10 até 1?\n" +
+              "1) for (int i = 1; i <= 10; i++)\n" +
+              "2) for (int i = 10; i >= 1; i--)\n" +
+              "3) for (int i = 0; i < 10; i++)\n" +
+              "4) for (int i = 10; i == 1; i--)\n" +
+              "5) for (int i = 1; i >= 10; i++)\n" +
+              "6) for (int i = 10; i >= 1; i++)\n";
+
+      String p6 = "\n[Capítulo 5, pág. 177] O que esse código faz? for (int i = 0; i < 5; i++) { System.out.println(\"Olá\"); }\n" +
+              "1) Mostra “Olá” uma vez\n" +
+              "2) Mostra “Olá” cinco vezes\n" +
+              "3) Não mostra nada\n" +
+              "4) Dá erro porque falta i++\n" +
+              "5) Mostra “Olá” infinitamente\n" +
+              "6) Mostra “Olá” 4 vezes\n";
+
+      String p7 = "\n[Capítulo 5, pág. 177] O que acontece com for (int i = 1; i > 5; i++)?\n" +
+              "1) Mostra os números de 1 a 5\n" +
+              "2) Imprime só 1\n" +
+              "3) Dá erro de compilação\n" +
+              "4) Não imprime nada\n" +
+              "5) Mostra infinitamente\n" +
+              "6) Mostra apenas 5\n";
+
+      String p8 = "\n[Capítulo 5, pág. 179] Qual laço soma os números de 1 a 5?\n" +
+              "1) for (int i = 5; i >= 1; i++)\n" +
+              "2) for (int i = 1; i <= 5; i++) soma += i;\n" +
+              "3) for (i = 1; i < 5; i--) soma += i;\n" +
+              "4) while (i < 5) soma += i;\n" +
+              "5) for (int i = 1; i <= 5; i--) soma -= i;\n" +
+              "6) for (int i = 0; i <= 5; i++) print soma;\n";
+
+      String p9 = "\n[Capítulo 5, pág. 174] Para que serve o i++ no for?\n" +
+              "1) Iniciar a variável i\n" +
+              "2) Verificar a condição do laço\n" +
+              "3) Incrementar a variável a cada repetição\n" +
+              "4) Encerrar o laço\n" +
+              "5) Deixar o código mais bonito\n" +
+              "6) Declarar uma nova função\n";
+
+      String p10 = "\n[Capítulo 5, pág. 172] Qual situação é ideal para usar o laço for?\n" +
+              "1) Quando não sabemos quantas vezes vamos repetir\n" +
+              "2) Quando temos que repetir algo para sempre\n" +
+              "3) Quando queremos repetir algo um número exato de vezes\n" +
+              "4) Quando queremos testar uma condição lógica\n" +
+              "5) Quando queremos repetir só se o usuário quiser\n" +
+              "6) Quando usamos apenas switch\n";
+   }
+   public void perguntasN8(){
+      String p1 = "\n[Capítulo 7, pág. 262]\n" +
+              "Para que serve a classe ArrayList em Java?\n" +
+              " 1) Criar gráficos\n" +
+              " 2) Armazenar muitos elementos que podem crescer automaticamente\n" +
+              " 3) Criar botões na tela\n" +
+              " 4) Repetir ações com for\n" +
+              " 5) Fazer contas matemáticas\n" +
+              " 6) Criar um banco de dados";
+      String p2 = "\n[Capítulo 7, pág. 263]\n" +
+              "Como se cria um ArrayList de Strings?\n" +
+              " 1) ArrayList<String> nomes = new ArrayList<>();\n" +
+              " 2) ArrayList nomes = \"String\";\n" +
+              " 3) String[] nomes = ArrayList();\n" +
+              " 4) ArrayList<nome> = new String();\n" +
+              " 5) ArrayList = {\"nome\"};\n" +
+              " 6) ArrayList new = String[];";
+      String p3 = "\n[Capítulo 7, pág. 264]\n" +
+              "Qual método adiciona um item a um ArrayList?\n" +
+              " 1) addItem()\n" +
+              " 2) append()\n" +
+              " 3) push()\n" +
+              " 4) add()\n" +
+              " 5) insert()\n" +
+              " 6) put()";
+      String p4 = "\n[Capítulo 7, pág. 265]\n" +
+              "Qual comando remove um elemento do ArrayList?\n" +
+              " 1) remove(index)\n" +
+              " 2) delete()\n" +
+              " 3) pop()\n" +
+              " 4) erase()\n" +
+              " 5) clear(index)\n" +
+              " 6) cut()";
+      String p5 = "\n[Capítulo 7, pág. 266]\n" +
+              "O que faz o método size()?\n" +
+              " 1) Mostra o conteúdo do ArrayList\n" +
+              " 2) Conta quantos itens há no ArrayList\n" +
+              " 3) Apaga o ArrayList\n" +
+              " 4) Adiciona um novo item\n" +
+              " 5) Ordena os itens\n" +
+              " 6) Mostra o último item";
+      String p6 = "\n[Capítulo 7, pág. 267]\n" +
+              "Como acessar o primeiro item de um ArrayList chamado nomes?\n" +
+              " 1) nomes[1]\n" +
+              " 2) nomes(0)\n" +
+              " 3) nomes.first()\n" +
+              " 4) nomes.get(0)\n" +
+              " 5) nomes.top()\n" +
+              " 6) nomes.index(1)";
+      String p7 = "\n[Capítulo 7, pág. 268]\n" +
+              "O que acontece se tentar acessar um índice que não existe?\n" +
+              " 1) O Java cria esse índice\n" +
+              " 2) Ele ignora e continua\n" +
+              " 3) Ele imprime 'null'\n" +
+              " 4) Dá erro em tempo de execução\n" +
+              " 5) O Java coloca um valor vazio lá\n" +
+              " 6) Ele reinicia o programa";
+      String p8 = "\n[Capítulo 7, pág. 269]\n" +
+              "Como limpar todos os itens de um ArrayList?\n" +
+              " 1) nomes.empty()\n" +
+              " 2) nomes.removeAll()\n" +
+              " 3) nomes.clear()\n" +
+              " 4) nomes.deleteAll()\n" +
+              " 5) nomes.erase()\n" +
+              " 6) nomes.cut()";
+      String p9 = "\n[Capítulo 7, pág. 270]\n" +
+              "Qual método substitui um valor em um ArrayList?\n" +
+              " 1) update()\n" +
+              " 2) change()\n" +
+              " 3) modify()\n" +
+              " 4) set(index, valor)\n" +
+              " 5) replace()\n" +
+              " 6) switch()";
+      String p10 = "\n[Capítulo 7, pág. 271]\n" +
+              "Como verificamos se um item está no ArrayList?\n" +
+              " 1) nomes.has(\"João\")\n" +
+              " 2) nomes.exists(\"João\")\n" +
+              " 3) nomes.contains(\"João\")\n" +
+              " 4) nomes.find(\"João\")\n" +
+              " 5) nomes.locate(\"João\")\n" +
+              " 6) nomes.check(\"João\")";
+
+   }
+
 }
